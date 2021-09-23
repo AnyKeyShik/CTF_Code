50e11956882a3fafcae74eb9b63a9a01d7205680
AnyKeyShik <nikitav59@gmail.com>
2021-09-24 01:06:16 +0300

    Fix crypto_medium2

--- crypto/medium2/server.py
+++ crypto/medium2/server.py
@@ -4,22 +4,16 @@
 import socket
 from secretsharing import SecretSharer
 
-# Server address
-SERVER_ADDRESS = ('', 1488)
-
-# Number of secret shards
-SHARDS = 5
-
-# Number of shards what will be send to user
-SEND_SHARDS = 3
-
-# Challenge text
-CHALL_TEXT = "Here's a base64-encoded and encrypted flag: {flag}\nYou need a secret and literally zero ivent to get it!\n\nIt's your three part:\n"
 
 def main():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(SERVER_ADDRESS)
-    server_socket.listen(50)
+    # Number of secret shards
+    SHARDS = 5
+
+    # Number of shards what will be send to user
+    SEND_SHARDS = 3
+
+    # Challenge text
+    CHALL_TEXT = "Here's a base64-encoded and encrypted flag: {flag}\nYou need a secret and literally zero ivent to get it!\n\nIt's your three part:"
 
     # Get encrypted flag
     with open('flag.enc', 'r') as flagfile:
@@ -29,13 +23,13 @@ def main():
     with open('key', 'r') as key:
         AES_KEY = key.read()
 
-    while True:
-        connection, address = server_socket.accept()
-        shares = SecretSharer.split_secret(AES_KEY, SEND_SHARDS, SHARDS)
+    shares = SecretSharer.split_secret(AES_KEY, SEND_SHARDS, SHARDS)
 
-        connection.send(CHALL_TEXT)
-        for i in range(SEND_SHARDS):
-            current_part = i + 1
-            connection.send("Part {part_num}: {part}\n".format(part_num=current_part, part=shares[i]))
+    print(CHALL_TEXT)
+    for i in range(SEND_SHARDS):
+        current_part = i + 1
+        print("Part {part_num}: {part}".format(part_num=current_part, part=shares[i]))
 
-        connection.close()
+
+if __name__ == "__main__":
+    main()
